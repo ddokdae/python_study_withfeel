d2dc72b61983ed091a5c1741216becd7a172c753
ddokdae <112316400+ddokdae@users.noreply.github.com>
2022-08-28 18:27:22 +0900

    20220828
    형변환
    문자열 인덱싱과 슬라이싱..을 배워따..

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+age=29
+print(age)
+정수형=18
+문자열='장은희'
+실수형=155.1
+list=[1, 2, 3]
+dict={"장필":"10,000","할리스":"100"}
+print(정수형)
+print('문자열') #얘는 따옴표를 붙여서 그냥 문자열로 나오는거임
+print(문자열)
+print(실수형)
+print(list)
+print(list[0])
+print(dict)
+print(dict.keys())
+print(dict.values())
+print(정수형, 실수형, 문자열, list, dict)
+print(8+5)
